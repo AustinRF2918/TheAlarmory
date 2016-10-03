eb2c1a00148db32fd75cff81f880d3df8545fdd5
Austin <austinrf2918@yahoo.com>
2016-10-03 13:03:12 -0400

    Added rough documentation to SelectorButtonComponent, fix up documentation a bit and add it to all other components.

--- js/selector-button-component.js
+++ js/selector-button-component.js
@@ -1,6 +1,9 @@
 var TimeFormatter = require('../js/time-formatting.js').TimeFormatter;
 var templateWrapper = require('../js/template-helpers.js').templateWrapper;
 
+/*
+  Put into own component, keep DRY
+*/
 function _checkForType( item, type, errorText ) {
     if ( typeof item !== type ) {
 	throw TypeError( errorText );
@@ -17,17 +20,25 @@ var SelectorButtonComponent = function( DOMId, number, className, nonNumeric){
     _checkForType( className, "string", "Invalid data type passed to TimeDeltaComponent parameter 3 (number) : must be a string.");
 
     return ( function( ) {
+	// CoreComponent
 	var _internalDOMIdentifier = DOMId + '-' + number;
 	var $el = $(_internalDOMIdentifier);
 
+	// CoreComponentFields
 	var _className = className;
 	var _isActive = false;
 	var _number = number;
 
+	// Handles Events
 	var _actions = [];
 
+	// Node
 	var _children = [];
 	var _parent = undefined;
+
+	var _active = ( function( ) {
+	    return _isActive;
+	} );
 
 	var _generateIdentifier = function( ) {
 	    if ( _isActive ) {
@@ -37,10 +48,16 @@ var SelectorButtonComponent = function( DOMId, number, className, nonNumeric){
 	    }
 	};
 
-	var _active = ( function( ) {
-	    return _isActive;
-	} );
+	/*
+	  View components: _render should only be called on initial render and following this
+	  a rerender should be called: JQuery will do this by changing text elements or any 
+	  class attributes.
+	*/
 
+	/*
+	  _generateTemplate: Internal function for render: The creates the HTML markup
+	  for our component. Core view trait requires this to be implemented.
+	*/
 	var _generateTemplate = function( ) {
 	    var tag = '';
 	    tag += _generateIdentifier( ) ;
@@ -49,17 +66,21 @@ var SelectorButtonComponent = function( DOMId, number, className, nonNumeric){
 	    return tag;
 	};
 	
+	/*
+	  _render: Internal function to remove the element that is current hooked to 
+	  our component and following this rerender the HTML: Unless there is some 
+	  reason to do so, we should in general keep this used only once, otherwise
+	  we should hook JQuery functions to our _handle function, implemented by
+	  the ComponentMessanger trait.
+	*/
 	var _render = function() {
 	    $el.remove( );
 	    $el.html( _generateTemplate( ) );
 	};
 
+	// Simple internal function to handle events.
 	var _pushActiveNumber = function( number ) {
 	    _isActive = function(){ return (number === _number) }();
-	};
-
-	var _pushParent = function( component ) {
-	    _parent = component;
 	};
 
 	var _update = function( currentNumberActive ) {
@@ -70,6 +91,17 @@ var SelectorButtonComponent = function( DOMId, number, className, nonNumeric){
 	    }
 	};
 
+	/*
+	  OrderedComponents trait: OrderedComponents will require our object to both
+	  implement a _pushParent function which our component can notify, and a 
+	  _pushChild function, which our component can receive notification from.
+	*/
+	var _pushParent = function( component ) {
+	    _parent = component;
+	};
+
+	// Debug function: Because this is ES6 syntax, we should disable these on
+	// deployment of the website.
 	var _display = function ( ) {
 	    console.log( `_className: (${_className})` );
 	    console.log( `_isActive: (${_isActive})` );
@@ -78,10 +110,28 @@ var SelectorButtonComponent = function( DOMId, number, className, nonNumeric){
 	    console.log( `html: (${_generateTemplate()})` );
 	};
 
+	/* 
+	   HandlesEvents trait: HandlesEvents makes us implement three functions:
+	   _pushAction, _setEvent, and _fireEvent. These all function to allow us
+	   to push lambdas  to evaluate when an event is fired (possible multiple)
+	   and then set the event that will fire these lambdas.
+	*/
+
+	/*
+	  _pushAction: pushAction takes an evaluatable function and pushes it to 
+	  a stack of internal actions: Later on we can set our event that will allow
+	  these actions to fire: naturally these actions allow extensibility to 
+	  our component and their behavior.
+	*/
 	var _pushAction = function ( fn ) {
 	    _actions.push(fn);
 	};
 
+	/*
+	  _setEvent: setEvent uses JQuery internally to give an event that will 
+	  fire our actions. For example: if we set our event as 'click', all 
+	  of our actions will fire when the specific components $el is clicked.
+	*/
 	var _setEvent = function ( event ) {
 	    $el.on( event, function() {
 	        _isActive = true;
@@ -94,6 +144,12 @@ var SelectorButtonComponent = function( DOMId, number, className, nonNumeric){
 	    });
 	};
 
+	/*
+	  _fireEvent: fireEvent is a debug function that allows us to actually 
+	  fire and event without actually have a JQuery event specified: this
+	  means in test frameworks like Mocha we don't have to implement 
+	  JQuery ourselves.
+	*/
 	var _fireEvent = function( ) {
 	    _isActive = true;
 
@@ -104,12 +160,32 @@ var SelectorButtonComponent = function( DOMId, number, className, nonNumeric){
 	    _notify( );
 	};
 
+	/* 
+	   Node trait: Node means we have to implement _notify and _handle:
+	   both of these allow our component to send and recieve events 
+	   and messages from other components respectively.
+	*/
+
+
+	/* 
+	   _notify: notify sends our parent a handle function with a JavaScript
+	   object: our parent is then free to implement handle in whatever way
+	   it sees fit to handle messaging between components. 
+	*/
 	var _notify = function ( ) {
 	    _parent.__handle( {
 		active: _isActive,
 		id: _number
 	    } );
 	};
+
+	/* 
+	   _handle: _handle will take a JavaScript object and analyize it to do 
+	   whatever actions it needs to do.
+	*/
+	var _handle = function( data ) {
+	    //pass.
+	}
 
 	return {
 	    __isActive: _active,
@@ -122,6 +198,7 @@ var SelectorButtonComponent = function( DOMId, number, className, nonNumeric){
 	    __render: _render,
 	    __parent: _parent,
 	    __notify: _notify,
+	    __handle: _handle,
 	    __fireEvent: _fireEvent,
 	    __componentName: "SelectorButtonComponent",
 	    setEvent: _setEvent,

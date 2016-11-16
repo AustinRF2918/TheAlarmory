ddf14a2ad3fe7d4db58626980d8cf2d987d4723d
Austin <austinrf2918@yahoo.com>
2016-11-15 19:43:29 -0500

    Cleaned up.

--- js/services/video-service.js
+++ js/services/video-service.js
@@ -3,61 +3,56 @@ var VideoService = function( ) {
 	var _cached;
 	var _currentRequest;
 
-	var _sendId = function( id ) {
-	    var url = null;
-	    try {
+	var _resetState = function( ) {
+	    if (_cached) {
 		_currentRequest.abort();
-	    } catch(e) {
 	    }
+	}
+
+	var _sendId = function( id ) {
+	    _resetState();
 
 	    _currentRequest = jQuery.ajax({
-	    url: 'http://127.0.0.1:5000/v1/id/' + id,
-	    dataType: 'json',
-	    success: function (json) {
-		try {
-		    if (json.toString() !== '') {
+		url: 'http://127.0.0.1:5000/v1/id/' + id,
+		dataType: 'json',
+		success: function ( json ) {
+		    if ( json.toString() ) {
 			_cached = json.toString();
 		    }
-		} catch(e) {
 		}
-	    }
 	    });
 	}
 
 	var _sendParameters = function( parameters ) {
-	    try {
-		_currentRequest.abort();
-	    } catch(e) {
-	    }
+	    _resetState();
 
 	    _currentRequest = jQuery.ajax({
 		url: 'http://127.0.0.1:5000/v1/video/' + parameters.join('+'),
 		dataType: 'json',
 		success: function (json) {
-		    try {
-			if (json.toString() !== '') {
-			    _cached = json.toString();
-			}
-		    } catch(e) {
+		    if ( json.toString() ) {
+			_cached = json.toString();
 		    }
 		}
 	    });
 	}
 
+	var _getPending = function( ) {
+	    try {
+		if ( !_cached ) {
+		    return null;
+		} else {
+		    return _cached;
+		}
+	    } catch( e ) {
+		return null;
+	    }
+	}
+
 	return {
 	    sendId: _sendId,
 	    sendParameters: _sendParameters,
-	    getPending: function() {
-		try {
-		    if (_cached === undefined) {
-			return null;
-		    } else {
-			return _cached;
-		    }
-		} catch (e) {
-		    return null;
-		}
-	    }
+	    getPending: _getPending
 	};
     } )();
 };

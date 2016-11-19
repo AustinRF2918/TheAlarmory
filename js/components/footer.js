c451ab36a62d6f0f1edba79264cc1465bd1ed918
Austin <austinrf2918@yahoo.com>
2016-11-18 20:11:05 -0500

    Passing tests.

--- js/components/footer.js
+++ js/components/footer.js
@@ -12,11 +12,6 @@ var FooterComponent = function( DOMId ){
 	var _internalDOMIdentifier = DOMId;
 	var $el = $(_internalDOMIdentifier);
 
-	// CoreComponentFields
-	var _hourActive = 12;
-	var _minuteActive = 0;
-	var _periodActive = "AM";
-
 	// Handles Events
 	var _actions = [];
 
@@ -47,14 +42,16 @@ var FooterComponent = function( DOMId ){
 	for our component. Core view trait requires this to be implemented.
 	*/
 	var _generateTemplate = function( ) {
+
 	    var tag = '';
-	    tag += '<div class="col-md-8" id="' + _internalDOMIdentifier + '">';
-
-	    for ( var i = 0; i < _children.length; i++) {
-		tag += _children[i].__generateTemplate();
-	    }
-
-	    tag += '</div>';
+	    tag += '<div class="inline-section">'
+	    tag +=   '<div class="col-lg-8">'
+	    tag +=     '<input type="video" class="form-control footer footer-red" id="video-form" placeholder="Paste a video URL here to wake up to it!"/>'
+	    tag +=   '</div>'
+	    tag +=   '<div class="col-lg-4">'
+	    tag +=     '<a class="btn header footer footer-blue" href="http://www.bluebikedesign.com">Created By Blue Bike Web Design</a>'
+	    tag +=   '</div>'
+	    tag += '</div>'
 	    return tag;
 	};
 
--- test/test-footer.js
+++ test/test-footer.js
@@ -4,80 +4,30 @@ describe('Full Footer', function() {
     describe('Properly constructs', function() {
 	it ('Should construct on demand.', function() {
 	    var footer = FooterComponent( "page-footer" );
-	    expect
+	    expect(footer.__generateTemplate()).to.equal('<div class="inline-section"><div class="col-lg-8"><input type="video" class="form-control footer footer-red" id="video-form" placeholder="Paste a video URL here to wake up to it!"/></div><div class="col-lg-4"><a class="btn header footer footer-blue" href="http://www.bluebikedesign.com">Created By Blue Bike Web Design</a></div></div>');
 	});
 
 	it ('Should throw when no DOM element is passed.', function() {
 	    expect( function() {
-		var tdsAlt = ControlPanelComponent( );
+		var footer = FooterComponent( );
 	    }).to.throw( );
 	});
 
 	it ('Should not throw when invalid numerics are passed.', function() {
 	    expect( function() {
-		var tdsAlt = ControlPanelComponent( 2 );
+		var footer = FooterComponent( 2 );
 	    }).to.throw( );
 	});
 
 	it ('Should have internal identifiers accessable.', function() {
-	    expect( tds.__DOMIdentifier != undefined ).to.be.true;
+	    var footer = FooterComponent( "Hello" );
+	    expect( footer.__DOMIdentifier != undefined ).to.be.true;
 	});
 
 	it ('Should be differentiatable.', function() {
-	    expect( tds !== tds2 ).to.be.true;
-	});
-    });
-
-    describe('Renders correctly.', function() {
-	it ('Should be able to render an empty component.', function() {
-	    var tds = ControlPanelComponent( "control-panel" );
-	    expect( tds.__generateTemplate() === '<div class="col-md-8" id="control-panel"></div>' ).to.be.true;
-	});
-
-	it ('Should be able to add a generic renderable component and render it alongside.', function() {
-	    var tdsA = SelectorPortionComponent( "hour-container", "Hours" );
-	    var tdsB = SelectorButtonComponent( "test", 0, "btn-square-hours" );
-	    var tds = ControlPanelComponent( "control-panel" );
-	    tdsA.pushChild( tdsB );
-	    tds.pushChild( tdsA );
-
-	    expect( tds.__generateTemplate() === '<div class="col-md-8" id="control-panel"><div class="row"><h3 class="text-body">Hours</h3></div><div class="row"><div class="square-container"><div id="test-0" class="btn-square-hours"><a>0</a></div></div></div></div>' ).to.be.true;
-
-	});
-
-	it ('Should be able to render multiple generic components.', function() {
-	    var tdsA = SelectorPortionComponent( "hour-container", "Hours" );
-	    var tdsB = SelectorButtonComponent( "test", 0, "btn-square-hours" );
-	    tdsA.pushChild( tdsB );
-
-	    var tdsC = SelectorPortionComponent( "minute-container", "Minutes" );
-	    var tdsD = SelectorButtonComponent( "test", 0, "btn-square-minutes" );
-	    tdsC.pushChild( tdsD );
-
-	    var tds = ControlPanelComponent( "control-panel" );
-	    tds.pushChild( tdsA );
-	    tds.pushChild( tdsC );
-
-	    expect( tds.__generateTemplate() === '<div class="col-md-8" id="control-panel"><div class="row"><h3 class="text-body">Hours</h3></div><div class="row"><div class="square-container"><div id="test-0" class="btn-square-hours"><a>0</a></div></div></div><div class="row"><h3 class="text-body">Minutes</h3></div><div class="row"><div class="square-container"><div id="test-0" class="btn-square-minutes"><a>0</a></div></div></div></div>' ).to.be.true;
-
-	});
-    });
-
-    describe('Performs component logic.', function() {
-	it ('Should be able to determine that after an action fires, something is active..', function() {
-	    var tds = SelectorPortionComponent( "hour-container", "Hours" );
-	    var tdsList = [];
-
-	    for (var i = 0; i < 12; i++) {
-		tdsList.push(SelectorButtonComponent( "#testa-" + i, i, "btn-square-hours" ));
-	    }
-
-	    for ( var i = 0; i< tdsList.length; i++ ) {
-		tds.pushChild(tdsList[i]);
-	    }
-
-	    tdsList[1].__fireEvent();
-	    tdsList[0].__fireEvent();
+	    var footerFirst = FooterComponent( "Hello" );
+	    var footerSecond = FooterComponent( "World" );
+	    expect( footerFirst !== footerSecond ).to.be.true;
 	});
     });
 });
